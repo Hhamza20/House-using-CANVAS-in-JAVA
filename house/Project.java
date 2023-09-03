b7ff4c975c256fb02b9556acba243e08229519b2
Hhamzabosss <94759932+Hhamzabosss@users.noreply.github.com>
2023-09-03 16:19:23 +0500

    Add files via upload

---
+++ house/Project.java
@@ -0,0 +1,357 @@
+public class Project {
+    public static void main(String[] args) {
+        Square sq1= new Square();
+        sq1.changeSize(600);
+        sq1.changeColor("black");
+        sq1.moveVertical(-200);
+        sq1.moveHorizontal(-400);
+        sq1.makeVisible();
+
+        Circle c1 = new Circle();
+        c1.changeSize(50);
+        c1.changeColor("white");
+        c1.moveHorizontal(-180);
+        c1.moveVertical(-70);
+        c1.makeVisible();
+
+        Circle c2 = new Circle();
+        c2.changeSize(50);
+        c2.changeColor("black");
+        c2.moveHorizontal(-160);
+        c2.moveVertical(-75);
+        c2.makeVisible();
+
+        Square sq2=new Square();
+        sq2.changeColor("white");
+        sq2.changeSize(100);
+        sq2.moveHorizontal(-50);
+        sq2.moveVertical(40);
+        sq2.makeVisible();
+
+        Triangle t1=new Triangle();
+        t1.changeSize(70,100);
+        t1.changeColor("magenta");
+        t1.moveVertical(-50);
+        t1.moveHorizontal(100);
+        t1.makeVisible();
+
+        Square sq3= new Square();
+        sq3.changeSize(35);
+        sq3.changeColor("green");
+        sq3.moveVertical(70);
+        sq3.moveHorizontal(10);
+        sq3.makeVisible();
+
+        Square sq4 = new Square();
+        sq4.changeSize(81);
+        sq4.moveVertical(60);
+        sq4.moveHorizontal(-80);
+        sq4.makeVisible();
+
+        Square sq5 = new Square();
+        sq5.changeSize(81);
+        sq5.moveVertical(60);
+        sq5.moveHorizontal(-120);
+        sq5.changeColor("white");
+        sq5.makeVisible();
+
+        Square sq6 = new Square();
+        sq6.changeSize(81);
+        sq6.moveVertical(60);
+        sq6.moveHorizontal(-131);
+        sq6.changeColor("black");
+        sq6.makeVisible();
+
+        Square sq7= new Square();
+        sq7.changeSize(14);
+        sq7.changeColor("yellow");
+        sq7.moveVertical(88);
+        sq7.moveHorizontal(13);
+        sq7.makeVisible();
+
+        Square sq8= new Square();
+        sq8.changeSize(14);
+        sq8.changeColor("yellow");
+        sq8.moveVertical(73);
+        sq8.moveHorizontal(13);
+        sq8.makeVisible();
+
+        Square sq9= new Square();
+        sq9.changeSize(14);
+        sq9.changeColor("yellow");
+        sq9.moveVertical(88);
+        sq9.moveHorizontal(28);
+        sq9.makeVisible();
+
+        Square sq10= new Square();
+        sq10.changeSize(14);
+        sq10.changeColor("yellow");
+        sq10.moveVertical(73);
+        sq10.moveHorizontal(28);
+        sq10.makeVisible();
+
+        Circle c3= new Circle();
+        c3.changeSize(10);
+        c3.moveVertical(130);
+        c3.moveHorizontal(45);
+        c3.changeColor("white");
+        c3.makeVisible();
+
+        Circle mini1= new Circle();
+        mini1.changeSize(3);
+        mini1.changeColor("yellow");
+        mini1.moveVertical(-50);
+        mini1.makeVisible();
+
+        Circle mini2= new Circle();
+        mini2.changeSize(5);
+        mini2.changeColor("yellow");
+        mini2.moveVertical(-55);
+        mini2.makeVisible();
+
+        Circle mini3= new Circle();
+        mini3.changeSize(3);
+        mini3.changeColor("yellow");
+        mini3.moveVertical(-45);
+        mini3.moveHorizontal(-10);
+        mini3.makeVisible();
+
+        Circle mini4= new Circle();
+        mini4.changeSize(5);
+        mini4.changeColor("yellow");
+        mini4.moveVertical(-40);
+        mini4.moveHorizontal(-20);
+        mini4.makeVisible();
+
+        Circle mini5= new Circle();
+        mini5.changeSize(5);
+        mini5.changeColor("yellow");
+        mini5.moveVertical(-40);
+        mini5.moveHorizontal(20);
+        mini5.makeVisible();
+
+        Circle mini6= new Circle();
+        mini6.changeSize(5);
+        mini6.changeColor("yellow");
+        mini6.moveVertical(-50);
+        mini6.moveHorizontal(30);
+        mini6.makeVisible();
+
+        Circle mini7= new Circle();
+        mini7.changeSize(3);
+        mini7.changeColor("yellow");
+        mini7.moveVertical(-50);
+        mini7.moveHorizontal(70);
+        mini7.makeVisible();
+
+        Circle mini8= new Circle();
+        mini8.changeSize(5);
+        mini8.changeColor("yellow");
+        mini8.moveVertical(-50);
+        mini8.moveHorizontal(80);
+        mini8.makeVisible();
+
+        Circle mini9= new Circle();
+        mini9.changeSize(5);
+        mini9.changeColor("yellow");
+        mini9.moveVertical(-40);
+        mini9.moveHorizontal(90);
+        mini9.makeVisible();
+
+        Circle mini10= new Circle();
+        mini10.changeSize(5);
+        mini10.changeColor("yellow");
+        mini10.moveVertical(-70);
+        mini10.moveHorizontal(-20);
+        mini10.makeVisible();
+
+        Circle mini11= new Circle();
+        mini11.changeSize(5);
+        mini11.changeColor("yellow");
+        mini11.moveVertical(-60);
+        mini11.moveHorizontal(40);
+        mini11.makeVisible();
+
+        Circle mini12= new Circle();
+        mini12.changeSize(5);
+        mini12.changeColor("yellow");
+        mini12.moveVertical(-45);
+        mini12.moveHorizontal(100);
+        mini12.makeVisible();
+
+        Circle mini13= new Circle();
+        mini13.changeSize(5);
+        mini13.changeColor("yellow");
+        mini13.moveVertical(-35);
+        mini13.moveHorizontal(125);
+        mini13.makeVisible();
+
+        Circle mini14= new Circle();
+        mini14.changeSize(5);
+        mini14.changeColor("yellow");
+        mini14.moveVertical(-50);
+        mini14.moveHorizontal(120);
+        mini14.makeVisible();
+
+        Circle mini15= new Circle();
+        mini15.changeSize(5);
+        mini15.changeColor("yellow");
+        mini15.moveVertical(-60);
+        mini15.moveHorizontal(100);
+        mini15.makeVisible();
+
+        Circle mini16= new Circle();
+        mini16.changeSize(5);
+        mini16.changeColor("yellow");
+        mini16.moveVertical(-50);
+        mini16.moveHorizontal(135);
+        mini16.makeVisible();
+
+        Circle mini17= new Circle();
+        mini17.changeSize(5);
+        mini17.changeColor("yellow");
+        mini17.moveVertical(-45);
+        mini17.moveHorizontal(-30);
+        mini17.makeVisible();
+
+        Circle mini18= new Circle();
+        mini18.changeSize(5);
+        mini18.changeColor("yellow");
+        mini18.moveVertical(-60);
+        mini18.moveHorizontal(145);
+        mini18.makeVisible();
+
+        Circle mini19= new Circle();
+        mini19.changeSize(5);
+        mini19.changeColor("yellow");
+        mini19.moveVertical(-35);
+        mini19.moveHorizontal(160);
+        mini19.makeVisible();
+
+        Circle mini20= new Circle();
+        mini20.changeSize(5);
+        mini20.changeColor("yellow");
+        mini20.moveVertical(-43);
+        mini20.moveHorizontal(163);
+        mini20.makeVisible();
+
+
+        Person p1=new Person();
+        Person p2=new Person();
+        p1.changeSize(60,30);
+        p1.changeColor("blue");
+        p1.moveHorizontal(-300);
+        p1.moveVertical(40);
+        p1.makeVisible();
+        p2.changeSize(60,30);
+        p2.changeColor("magenta");
+        p2.moveHorizontal(-340);
+        p2.moveVertical(40);
+        p2.makeVisible();
+        p1.slowMoveHorizontal(252);
+        p2.slowMoveHorizontal(255);
+
+        Person p3=new Person();
+        p3.changeSize(30,20);
+        p3.moveVertical(60);
+        p3.moveHorizontal(-300);
+        p3.changeColor("magenta");
+        p3.makeVisible();
+        p3.slowMoveHorizontal(185);
+
+        Person p4=new Person();
+        p4.changeSize(30,20);
+        p4.moveVertical(60);
+        p4.moveHorizontal(-300);
+        p4.changeColor("blue");
+        p4.makeVisible();
+        p4.slowMoveHorizontal(160);
+        p1.slowMoveHorizontal(60);
+        p1.slowMoveVertical(-20);
+        p1.makeInvisible();
+        p2.slowMoveHorizontal(100);
+        p2.slowMoveVertical(-20);
+        p2.makeInvisible();
+        p3.slowMoveHorizontal(130);
+        p3.slowMoveVertical(-20);
+        p3.makeInvisible();
+        p4.slowMoveHorizontal(155);
+        p4.slowMoveVertical(-20);
+        p4.makeInvisible();
+        sq7.changeColor("black");
+        sq8.changeColor("black");
+        sq9.changeColor("black");
+        sq10.changeColor("black");
+       // p2.makeInvisible()2
+
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+
+
+        sq1.changeColor("white");
+        c1.changeColor("yellow");
+
+        sq2.changeColor("yellow");
+        sq2.makeVisible();
+
+        t1.changeColor("magenta");
+        t1.makeVisible();
+
+        sq3.changeColor("green");
+        sq3.makeVisible();
+
+        sq4.makeVisible();
+
+        sq5.changeColor("yellow");
+        sq5.makeVisible();
+
+        sq6.changeColor("white");
+        sq6.makeVisible();
+
+        sq7.changeColor("yellow");
+        sq7.makeVisible();
+
+        sq8.changeColor("yellow");
+        sq8.makeVisible();
+
+        sq9.changeColor("yellow");
+        sq9.makeVisible();
+
+        sq10.changeColor("yellow");
+        sq10.makeVisible();
+
+        c3.makeVisible();
+
+        p1.makeVisible();
+        p1.slowMoveVertical(20);
+        p1.slowMoveHorizontal(-252);
+
+        p2.makeVisible();
+        p2.slowMoveVertical(20);
+        p2.slowMoveHorizontal(-220);
+
+        p3.makeVisible();
+        p3.slowMoveVertical(20);
+        p3.slowMoveHorizontal(-185);
+
+        p4.makeVisible();
+        p4.slowMoveVertical(20);
+        p4.slowMoveHorizontal(-160);
+
+//        Person p5=new Person();
+//        p5.changeSize(30,20);
+//        //p5.moveVertical(60);
+//       // p5.moveHorizontal(-300);
+//        p5.changeColor("blue");
+//        p5.moveVertical(40);
+//        p5.moveHorizontal(18);
+//        p5.makeVisible();
+//        p5.slowMoveVertical(20);
+//        p5.slowMoveHorizontal(-135);
+
+    }
+}
